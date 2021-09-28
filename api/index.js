cd5a07251f61c98ec4dba635b1fd8edceb3d7fb5
Yazid Lee <YazidLee@users.noreply.github.com>
2021-09-28 14:01:34 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,18 +2,21 @@ const axios = require('axios') // 发送 REST 请求
 
 module.exports = (req, res) => {
     let result = {}
-    try {
-        console.time("QQ-Avatar")
-        console.log("开始获取头像")
-        axios.get("https://ssl.ptlogin2.qq.com/getface?imgtype=4&uin=302834972", { timeout: 10000 }).then(resp => {
-            result = resp
-            console.log("头像获取成功")
-            console.timeEnd("QQ-Avatar")
-        }).catch(e => {
-            console.error('获取 QQ 头像失败：', e)
-        })     
-    } catch(e) {
-        console.error('获取 QQ 头像失败：', e)
-    }
+    result = await getQQAvatar("302834972@qq.com")
     res.status(200).json(result)
+}
+
+async function getQQAvatar (qq) {
+  try {
+    const qqNum = qq.replace(/@qq.com/ig, '')
+    const result = await axios.get(`https://ssl.ptlogin2.qq.com/getface?imgtype=4&uin=${qqNum}`, { timeout: 5000 })
+    if (result && result.data) {
+      const start = result.data.indexOf('http')
+      const end = result.data.indexOf('"', start)
+      if (start === -1 || end === -1) return null
+      return result.data.substring(start, end)
+    }
+  } catch (e) {
+    console.error('获取 QQ 头像失败：', e)
+  }
 }
